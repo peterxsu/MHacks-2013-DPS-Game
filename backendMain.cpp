6855795b8342ce78fce3f84bed742af90f61bcf1
scottxsu <scottxsu@umich.edu>
2013-02-03 00:39:02 -0500

    moved everything into functions

--- backendMain.cpp
+++ backendMain.cpp
@@ -21,7 +21,17 @@ bool upKeyDown = false;
 bool leftKeyDown = false;
 bool downKeyDown = false;
 bool rightKeyDown = false;
+Display display(250, 150);
+Player p(340, 240, 100, 1, 1000);
+vector<Enemy> enemies;
+vector<Bullet*> bullets;
+bool tutorial = true;
+fstream save;
+time_t prevTime, prevTime2, prevTime3, prevTime4;
 
+void initialize();
+void endTutorial(fstream&);
+void gameActions();
 string expandMapUp(fstream&);
 string expandMapRight(fstream&);
 string expandMapDown(fstream&);
@@ -34,22 +44,35 @@ void addMapLeft(fstream&, string);
 
 DWORD WINAPI backendMain(LPVOID lpParam)
 {
+	//detect keyboard presses and do appropriate action
+	//automatically move enemies and bullets
+	
+	/*save.close();
+	save.open("data.sav", fstream::in | fstream::app);
+	for (int x = 0; x < 10; ++x)
+	{
+		addMapUp(save, expandMapUp(save));
+		addMapDown(save, expandMapDown(save));
+		addMapLeft(save, expandMapLeft(save));
+		addMapRight(save, expandMapRight(save));
+	}
+	display.updateDisplay(enemies, bullets);
+	save.close();*/
+	return 0;
+}
+
+void initialize()
+{
 	srand(time(NULL));
-	//create player, enemy, bullets
-	Player p(340, 240, 100, 1, 1000);
-	vector<Enemy> enemies;
-	vector<Bullet*> bullets;
-	bool tutorial = true;
 	//create the two enemies that will be a part of the tutorial
 	Enemy tutor1(200, 120, 5, 0);
 	Enemy tutor2(480, 360, 5, 0);
 	enemies.push_back(tutor1);
 	enemies.push_back(tutor2);
 	//edit the .sav file
-	fstream save;
 	save.open("data.sav", fstream::out | fstream::trunc);
 	if (!save.is_open())
-		;//probably should throw an exception
+		exit(1);//probably should throw an exception
 	for (int x = 0; x < 25; ++x)
 	{
 		for (int y = 0; y < 120; ++y)
@@ -77,11 +100,115 @@ DWORD WINAPI backendMain(LPVOID lpParam)
 			save << '0';
 		save << '\n';
 	}
-	Display display(250, 150);
 	display.updateDisplay(enemies, bullets);
-	
-	//detect keyboard presses and do appropriate action
-	//automatically move enemies and bullets
+}
+
+void gameActions()
+{
+	if (difftime(time(NULL), prevTime) > 0.1)
+	{
+		prevTime = time(NULL);
+		for (int x = 0; x < (int) bullets.size(); ++x)
+			bullets[x]->move();
+		for (int x = 0; x < (int) enemies.size(); ++x)
+			enemies[x].move(p.getX(), p.getY());
+		if (wKeyDown)
+		{
+			display.moveDisplay(0, 1);
+			for (int x = 0; x < (int) bullets.size(); ++x)
+				bullets[x]->move(0, 1);
+			for (int x = 0; x < (int) enemies.size(); ++x)
+				enemies[x].move(0, 1, 0);
+				
+		}
+		else if (dKeyDown)
+		{
+			display.moveDisplay(-1, 0);
+			for (int x = 0; x < (int) bullets.size(); ++x)
+				bullets[x]->move(-1, 0);
+			for (int x = 0; x < (int) enemies.size(); ++x)
+				enemies[x].move(-1, 0, 0);
+		}
+		else if (sKeyDown)
+		{
+			display.moveDisplay(0, -1);
+			for (int x = 0; x < (int) bullets.size(); ++x)
+				bullets[x]->move(0, -1);
+			for (int x = 0; x < (int) enemies.size(); ++x)
+				enemies[x].move(0, -1, 0);
+		}
+		else if (aKeyDown)
+		{
+			display.moveDisplay(1, 0);
+			for (int x = 0; x < (int) bullets.size(); ++x)
+				bullets[x]->move(1, 0);
+			for (int x = 0; x < (int) enemies.size(); ++x)
+				enemies[x].move(1, 0, 0);
+		}
+	}
+	if (difftime(time(NULL), prevTime2) > 1)
+	{
+		prevTime2 = time(NULL);
+		for (int x = 0; x < (int) enemies.size(); ++x)
+			if (enemies[x].getStrength())
+				bullets.push_back(enemies[x].attack(p.getX(), p.getY()));
+	}
+	if (difftime(time(NULL), prevTime3) > 0.001*p.getCooldown())
+	{
+		prevTime3 = time(NULL);
+		if (upKeyDown)
+			bullets.push_back(p.attack(0));
+		else if (rightKeyDown)
+			bullets.push_back(p.attack(1));
+		else if (downKeyDown)
+			bullets.push_back(p.attack(2));
+		else if (leftKeyDown)
+			bullets.push_back(p.attack(3));
+	}
+	//should only happen when the tutorial is done so determine when the two tutorial enemies are destroyed
+	if (!tutorial && difftime(time(NULL), prevTime4) > 5)
+	{
+		int randonNum = rand() % 4;
+		prevTime4 = time(NULL);
+		int enemyX, enemyY;
+		if (randomNum == 0)
+		{
+			randomNum = rand() % 70;
+			enemyX = randomNum;
+			enemyY = 0;
+		}
+		else if (randomNum == 1)
+		{
+			randomNum = rand() % 50;
+			enemyX = 69;
+			enemyY = randomNum;
+		}
+		else if (randomNum == 2)
+		{
+			randomNum = rand() % 70;
+			enemyX = randomNum;
+			enemyY = 49;
+		}
+		else
+		{
+			randomNum = rand() % 50;
+			enemyX = 0;
+			enemyY = randomNum;
+		}
+		Enemy newEnemy(enemyX*10,enemyY*10,5,1);
+		enemies.push_back(newEnemy);
+	}
+	if (enemies.size() == 0 && tutorial)
+	{
+		endTutorial();
+		tutorial = false;
+		prevTime4 = time(NULL);
+	}
+	display.updateDisplay(enemies, bullets);
+}
+
+void endTutorial()
+{
 	save.close();
 	save.open("data.sav", fstream::out | fstream::trunc);
 	for (int x = 0; x < 80; ++x)
@@ -95,90 +222,6 @@ DWORD WINAPI backendMain(LPVOID lpParam)
 		}
 		save << '\n';
 	}
-	save.close();
-	save.open("data.sav", fstream::in | fstream::app);
-	for (int x = 0; x < 10; ++x)
-	{
-		addMapUp(save, expandMapUp(save));
-		addMapDown(save, expandMapDown(save));
-		addMapLeft(save, expandMapLeft(save));
-		addMapRight(save, expandMapRight(save));
-	}
-	display.updateDisplay(enemies, bullets);
-	time_t prevTime = time(NULL), prevTime2 = prevTime, prevTime3 = prevTime, prevTime4 = prevTime;
-	while (true)
-	{
-		//detect keyboard presses
-		if (difftime(time(NULL), prevTime) > 0.1)
-		{
-			prevTime = time(NULL);
-			for (int x = 0; x < (int) bullets.size(); ++x)
-				bullets[x]->move();
-			for (int x = 0; x < (int) enemies.size(); ++x)
-				enemies[x].move(p.getX(), p.getY());
-			if (wKeyDown)
-			{
-				display.moveDisplay(0, 1);
-				for (int x = 0; x < (int) bullets.size(); ++x)
-					bullets[x]->move(0, 1);
-				for (int x = 0; x < (int) enemies.size(); ++x)
-					enemies[x].move(0, 1, 0);
-			}
-			else if (dKeyDown)
-			{
-				display.moveDisplay(-1, 0);
-				for (int x = 0; x < (int) bullets.size(); ++x)
-					bullets[x]->move(-1, 0);
-				for (int x = 0; x < (int) enemies.size(); ++x)
-					enemies[x].move(-1, 0, 0);
-			}
-			else if (sKeyDown)
-			{
-				display.moveDisplay(0, -1);
-				for (int x = 0; x < (int) bullets.size(); ++x)
-					bullets[x]->move(0, -1);
-				for (int x = 0; x < (int) enemies.size(); ++x)
-					enemies[x].move(0, -1, 0);
-			}
-			else if (aKeyDown)
-			{
-				display.moveDisplay(1, 0);
-				for (int x = 0; x < (int) bullets.size(); ++x)
-					bullets[x]->move(1, 0);
-				for (int x = 0; x < (int) enemies.size(); ++x)
-					enemies[x].move(1, 0, 0);
-			}
-		}
-		if (difftime(time(NULL), prevTime2) > 1)
-		{
-			prevTime2 = time(NULL);
-			for (int x = 0; x < (int) enemies.size(); ++x)
-				if (enemies[x].getStrength())
-					bullets.push_back(enemies[x].attack(p.getX(), p.getY()));
-		}
-		if (difftime(time(NULL), prevTime3) > 0.001*p.getCooldown())
-		{
-			prevTime3 = time(NULL);
-			if (upKeyDown)
-				bullets.push_back(p.attack(0));
-			else if (rightKeyDown)
-				bullets.push_back(p.attack(1));
-			else if (downKeyDown)
-				bullets.push_back(p.attack(2));
-			else if (leftKeyDown)
-				bullets.push_back(p.attack(3));
-		}
-		//should only happen when the tutorial is done so determine when the two tutorial enemies are destroyed
-		if (difftime(time(NULL), prevTime4) > 5 && !tutorial)
-		{
-			prevTime4 = time(NULL);
-			Enemy newEnemy(0,0,5,1);
-			enemies.push_back(newEnemy);
-		}
-		display.updateDisplay(enemies, bullets);
-	}
-	save.close();
-	return 0;
 }
 
 string expandMapUp(fstream& fs)
